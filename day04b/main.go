6e0f8ac6f2ff2ab6f3d72fff3caf72b2fc84a0ed
andraspalasti <andraspalasti2@gmail.com>
2022-12-06 11:54:28 +0100

    completed day 4

---
+++ day04b/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func ParseRange(s string) [2]int {
+	i := strings.Index(s, "-")
+	if i == -1 {
+		panic("Unable to parse range")
+	}
+
+	l, err := strconv.Atoi(s[:i])
+	if err != nil {
+		panic("Unable to parse range")
+	}
+	h, err := strconv.Atoi(s[i+1:])
+	if err != nil {
+		panic("Unable to parse range")
+	}
+	return [2]int{l, h}
+}
+
+func main() {
+	count := 0
+	for _, line := range strings.Split(input, "\n") {
+		part1, part2, found := strings.Cut(line, ",")
+		if !found {
+			panic("Invalid line")
+		}
+		range1, range2 := ParseRange(part1), ParseRange(part2)
+
+		if range1[0] <= range2[1] && range2[1] <= range1[1] {
+			count++
+		} else if range2[0] <= range1[1] && range1[1] <= range2[1] {
+			count++
+		}
+	}
+
+	fmt.Println("Answear:", count)
+}
